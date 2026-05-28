69de7e49976c3fb27f1dbf0a873897650adf24b1
Jorge-Feelipe1496 <jorgefelipe1608@gmail.com>
2026-05-28 00:05:27 -0300

    Adicionei mais informações para o Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,34 @@
 public class Cliente {
     private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 
     public String getNome() {
         return nome;
